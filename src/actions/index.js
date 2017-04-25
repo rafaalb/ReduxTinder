558c76fd4e341fa621f22dbbe55c4a20646e0335
terra <rafaa.lb@hotmail.com>
2017-04-24 20:18:18 -0400

    added action creator and book reducer

---
+++ src/actions/index.js
@@ -0,0 +1,6 @@
+export function selectBook(book){
+	console.log('A book has been selected',book.title);
+}
+
+
+
---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,10 @@
+export default function(){
+
+	return [
+		{title:'Harry Potter'},
+		{title:'Javascript'},
+		{title:'Jurassic World'},
+		{title: 'The dark tower'}
+	];
+
+}
